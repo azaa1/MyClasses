3ca00821b7447973575176093ab90a7dafc283c2
azaa1 <azaa.chi11@gmail.com>
2019-07-06 12:04:15 -0500

    variable.c file added

--- CS50/c-programming/hello.c
+++ CS50/c-programming/hello.c
@@ -5,13 +5,3 @@ int main()
 {
    printf("Hello World!\n");
 }
-
-/*
-// print an interger 'count'
-int main()
-{
-int count = 23;
-   // printf() displays the string inside quotation
-   printf("%d", count);
-}
-*/
---
+++ CS50/c-programming/variable.c
@@ -0,0 +1,9 @@
+// print an interger 'count'
+#include<stdio.h>
+int main()
+{
+   // assigning a value (23) to interger 'count'
+int count = 23;
+   // printf() displays the string inside quotation
+   printf("%d", count);
+}
